14cfcee05153e2602afbf269a3f97a8e62a0b92c
Татьяна Пятыхина <tanyapyat@MacBook-Pro-Tatana.local>
2020-10-31 19:35:09 +0300

    composition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
-import { slider } from './sections/3.js';
+import { slider, composition } from './sections/3.js';
 import { verticalAccordeon } from './sections/4.js';
 import { horizontalAccordeon } from './sections/5.js';
 import { init } from './sections/8.js';
-
 import { onePageScroll } from './navigation.js';
 
 slider();
+composition();
 
 verticalAccordeon();
 horizontalAccordeon();
--- src/scripts/sections/3.js
+++ src/scripts/sections/3.js
@@ -21,7 +21,28 @@ export function slider () {
                     reqIndex = 0;
                 }
             }
+
             container.style.left = - reqIndex*100 + '%';
+            [].forEach.call(items, item => {
+                item.classList.remove('activeItem');
+            });
+            items[reqIndex].classList.add('activeItem');
+            composition();
         })
+    });
+}
+
+export function composition () {
+    const activeSlide = document.getElementsByClassName('activeItem')[0];
+    const trigger = activeSlide.getElementsByClassName('burgers__list-item-left-composition')[0];
+    const dropdown = activeSlide.getElementsByClassName('burgers__list-item-left-dropdown')[0];
+
+    dropdown.classList.remove('activeDropdown');
+    trigger.classList.remove('activeDropdownTrigger');
+
+    trigger.addEventListener('click', function(e) {
+        e.stopImmediatePropagation();
+        dropdown.classList.toggle('activeDropdown');
+        trigger.classList.toggle('activeDropdownTrigger');
     })
 }
